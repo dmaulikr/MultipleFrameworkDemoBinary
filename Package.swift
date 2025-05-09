4dc47dcee65df254af4c09b0427601d329a033bf
Maulik Desai <dmaulikr@gmail.com>
2025-05-09 11:08:16 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,22 +12,22 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "CoreKit",
-      url: "https://github.com/dmaulikr/MultipleFrameworkDemo/releases/download/1.0.0/CoreKit-1.0.0.zip",
+      url: "https://github.com/dmaulikr/MultipleFrameworkDemoBinary/releases/download/1.0.0/CoreKit-1.0.0.zip",
       checksum: "76c829678bc70693f8fba42af58cc4f571053e5a4a5b1a99e4fe8f31c08dd1d0"
     ),
     .binaryTarget(
       name: "FeatureA",
-      url: "https://github.com/dmaulikr/MultipleFrameworkDemo/releases/download/1.0.0/FeatureA-1.0.0.zip",
+      url: "https://github.com/dmaulikr/MultipleFrameworkDemoBinary/releases/download/1.0.0/FeatureA-1.0.0.zip",
       checksum: "3fe49d0248d4d36377c06f73c1ec9b07146e88696c84d95a98341e41948184d4"
     ),
     .binaryTarget(
       name: "FeatureB",
-      url: "https://github.com/dmaulikr/MultipleFrameworkDemo/releases/download/1.0.0/FeatureB-1.0.0.zip",
+      url: "https://github.com/dmaulikr/MultipleFrameworkDemoBinary/releases/download/1.0.0/FeatureB-1.0.0.zip",
       checksum: "2ec95735348c2b55bce4ccee67e822eec28a1596dffab8fe3fa4b3a38db4f218"
     ),
     .binaryTarget(
       name: "FeatureC",
-      url: "https://github.com/dmaulikr/MultipleFrameworkDemo/releases/download/1.0.0/FeatureC-1.0.0.zip",
+      url: "https://github.com/dmaulikr/MultipleFrameworkDemoBinary/releases/download/1.0.0/FeatureC-1.0.0.zip",
       checksum: "717548d3eadffe45e6835dea73dd85b46283c8f6a24c781058a0c20f681907ca"
     ),
   ]
